e23afee9296df14eb17dab91d81c3992c5cf8667
Arc-001 <25131347+Arc-001@users.noreply.github.com>
2025-04-16 14:11:07 +0530

    FEAT: Added basic operations

---
+++ utils/helper.py
@@ -0,0 +1,11 @@
+from utils import descriptive_stats as ds
+import numpy as np
+import scipy.stats as stats
+
+#TODO: implement different types of percentile
+
+def percentile(data: ds.data, x: int):
+    stats.percentileofscore(data.get_data(), x)
+
+
+def
